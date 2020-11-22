cb4a3b8e0ac0450c3674174aa6e0d3034584b0cd
Narsad <59451254+Narsad@users.noreply.github.com>
2020-11-22 19:34:54 +0400

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-const menuBurger = document.querySelector('.menu');
-const nav = document.querySelector('nav');
-
-menuBurger.addEventListener('click', function(){
-    nav.classList.toggle('nav');
-})
